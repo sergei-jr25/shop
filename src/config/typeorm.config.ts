8925cfc9007c63ec102363c9c0a6a7fbb81ce901
Ваше Имя <.email@example.com>
2024-02-27 23:44:25 +0300

    change server

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,25 @@ export const getTypeOrmConfig = async (
 	configServer: ConfigService
 ): Promise<TypeOrmModuleOptions> => ({
 	type: 'postgres',
-	host: 'dpg-cm2n5mmn7f5s73ehf1rg-a',
+	host: configServer.get('POSTGRES_HOST'),
+	database: configServer.get('POSTGRES_DATABASE'),
+	username: configServer.get('POSTGRES_USER'),
 	port: 5432,
-	database: 'shoppst',
-	username: 'sergeijr',
-	password: 'drisHCFJRKji0xh1phGg7ve64UGEbyfz',
+	password: configServer.get('POSTGRES_PASSWORD'),
 	autoLoadEntities: true,
-	synchronize: true
+	synchronize: true,
+	ssl: true
 })
+
+// export const getTypeOrmConfig = async (
+// 	configServer: ConfigService
+// ): Promise<TypeOrmModuleOptions> => ({
+// 	type: 'postgres',
+// 	host: 'localhost',
+// 	port: 5432,
+// 	database: 'shop-04-06',
+// 	username: 'postgres',
+// 	password: '12345',
+// 	autoLoadEntities: true,
+// 	synchronize: true
+// })
