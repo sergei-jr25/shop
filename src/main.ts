9773675268fdd449af228d2bf0424ea8a5d58152
Ваше Имя <.email@example.com>
2024-02-28 20:40:07 +0300

    cnhange localhost

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 	app.enableCors({
 		credentials: true,
 		origin: [
-			'http://localhost:300',
+			'http://localhost:3000',
 			'https://shop-mdk7.onrender.com',
 			'https://online-shop-client-37335w3eq-sergei-jr25.vercel.app'
 		]
