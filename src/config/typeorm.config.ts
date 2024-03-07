ca22d0c823068cedbb3edb25ab15247f5e0f5c8d
Ваше Имя <.email@example.com>
2024-03-07 21:16:16 +0300

    database

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,29 +1,29 @@
 import { ConfigService } from '@nestjs/config'
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 
-// export const getTypeOrmConfig = async (
-// 	configServer: ConfigService
-// ): Promise<TypeOrmModuleOptions> => ({
-// 	type: 'postgres',
-// 	host: configServer.get('POSTGRES_HOST'),
-// 	database: configServer.get('POSTGRES_DATABASE'),
-// 	username: configServer.get('POSTGRES_USER'),
-// 	port: 5432,
-// 	password: configServer.get('POSTGRES_PASSWORD'),
-// 	autoLoadEntities: true,
-// 	synchronize: true,
-// 	ssl: true
-// })
-
 export const getTypeOrmConfig = async (
 	configServer: ConfigService
 ): Promise<TypeOrmModuleOptions> => ({
 	type: 'postgres',
-	host: 'localhost',
+	host: configServer.get('POSTGRES_HOST'),
+	database: configServer.get('POSTGRES_DATABASE'),
+	username: configServer.get('POSTGRES_USER'),
 	port: 5432,
-	database: 'shop-04-06',
-	username: 'postgres',
-	password: '12345',
+	password: configServer.get('POSTGRES_PASSWORD'),
 	autoLoadEntities: true,
-	synchronize: true
+	synchronize: true,
+	ssl: true
 })
+
+// export const getTypeOrmConfig = async (
+// 	configServer: ConfigService
+// ): Promise<TypeOrmModuleOptions> => ({
+// 	type: 'postgres',
+// 	host: 'localhost',
+// 	port: 5432,
+// 	database: 'shop-04-06',
+// 	username: 'postgres',
+// 	password: '12345',
+// 	autoLoadEntities: true,
+// 	synchronize: true
+// })
